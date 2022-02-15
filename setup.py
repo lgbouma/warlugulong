0a4c3999897f3aceb5f9a718a9debac8cc5d7f99
lgbouma <bouma.luke@gmail.com>
2022-02-15 10:38:13 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='dreams of dispersing dots',
+    author='Luke Bouma',
+    license='MIT',
+)
